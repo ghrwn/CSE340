c62a5c30dff82b832b0d8608fa90b5deac9c702c
ghrwn <gaelaherwin@gmail.com>
2026-01-17 03:20:01 -0700

    commit w1

--- routes/static.js
+++ routes/static.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-// Serve static files from the public folder
-router.use(express.static("public"))
+// Serve static files ONLY
+router.use("/css", express.static("public/css"))
+router.use("/images", express.static("public/images"))
+router.use("/js", express.static("public/js"))
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,13 +7,20 @@
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
+ * Static Routes
  *************************/
 app.use(static)
 
@@ -23,13 +30,6 @@ app.use(static)
 app.get("/", function (req, res) {
   res.render("index", { title: "Home" })
 })
-
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Local Server Information
