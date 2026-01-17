eebd29df6f7764bf7219e0c546c773ee5a75f021
ghrwn <gaelaherwin@gmail.com>
2026-01-17 03:00:12 -0700

    commit w1

--- routes/static.js
+++ routes/static.js
@@ -1,10 +1,7 @@
-const path = require("path")
 const express = require("express")
 const router = express.Router()
 
+// Serve static files from the public folder
 router.use(express.static("public"))
-router.use("/css", express.static(path.join(__dirname, "../public/css")))
-router.use("/js", express.static(path.join(__dirname, "../public/js")))
-router.use("/images", express.static(path.join(__dirname, "../public/images")))
 
 module.exports = router
